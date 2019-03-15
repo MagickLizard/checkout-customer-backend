faa80966e22b993beef6f2100ed0d818255e4d53
Lizzie King <lizzieking521@gmail.com>
2019-03-15 14:35:26 +1100

    clean up

--- frontend/index.js
+++
@@ -1,6 +0,0 @@
-
-// registerServiceWorker(); TODO
-
-// const App = require('./client/src/index.jsx');
-const App = require('./client/public/index.html'); //unsure how to do this without specify path directly need to do research
-module.exports = new App();
