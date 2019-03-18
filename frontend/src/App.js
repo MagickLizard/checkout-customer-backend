4cd17990e29e914432c5fd6bf24856426ee1b64c
Lizzie King <lizzieking521@gmail.com>
2019-03-18 21:58:50 +1100

    favicon and image added for temp homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/header/header.js';
 import './App.css';
-import "bulma/sass/utilities/_all.sass"
-import "bulma/sass/grid/columns.sass"
 
 class App extends Component {
   render() {
     return (
-
       <div className="App"> 
-
-        <Header>
-          <p>
-            Welcome to lizard magic
-          </p>
-        </Header>
+        {/* <Header/> */}
         <h1> lizard magik </h1>
+        <img name="dragon" src="dragon.png" alt="dragon"/>
         <h2> A magical experience like no other. </h2>
       </div>
     );
