0803276edf74d3e6adb4eaed47ce1310e9df4d6e
Lizzie King <lizzieking521@gmail.com>
2019-04-18 17:09:39 +1000

    ADDED: dynamic building for message path

--- backend/services/api-request-service.js
+++ backend/services/api-request-service.js
@@ -3,7 +3,7 @@ const url = require('url');
 
 const API_BASE_URL = 'http://api.instagram.com';
 async function getRecentConversationSummaries() {
-  let makeRequest =  async (path) => {
+  let makeRequest = async (path) => {
     const options = {
       method: 'GET',
       url: path
@@ -11,7 +11,7 @@ async function getRecentConversationSummaries() {
     return new Promise((resolve, reject) => {
       request(options, (err, response, body) => {
         if (err) {
-          console.log('err>>>', err)
+          console.error(' Error 1*****', err);
           reject(err);
         }
         else {
@@ -21,26 +21,50 @@ async function getRecentConversationSummaries() {
     });
   };
   const conversationsPath = async setPathConversations => {
-    const path = API_BASE_URL + '/v1/tags/search/q:cat';
-    const conversationResult =  await makeRequest(path);
+    const path = API_BASE_URL + '/v1/tags/search/q:cat'; //try splitting these into one function and for loop around make request
+    const conversationResult = await makeRequest(path);
     return conversationResult;
   };
   const messagesPath = async setMessagesPath => {
-    const path = API_BASE_URL + '/v1/tags/search/q:lizard';
-    const messageResult =  await makeRequest(path);
-    return messageResult;
+    let promiseArray = [];
+    for (let i = 0; i < searchArray.length; i++) {
+      promiseArray.push(new Promise((resolve, reject) => {
+        let conversationId = JSON.stringify(searchArray[i].id);
+        let path = API_BASE_URL + '/v1/tags/search/q:lizard' + searchArray[i].id;
+        makeRequest(path)
+          .then(data => {
+            resolve(JSON.parse(data));
+          })
+          .catch(err => {
+            console.error(' Error 2*****', err);
+            reject(err);
+          });
+      }));
+    }
+    return Promise.all(promiseArray)
+      .then(promise => {
+        return (promise);
+      })
+      .catch(err => {
+        console.error('promise all error *****', err);
+        return (err);
+      });
   };
   const usersPath = async setUserPath => {
-    const path = API_BASE_URL + '/v1/tags/search/q:dog';
-    const userResult =  await makeRequest(path);
+    const userId = 1; //TODO
+    const path = API_BASE_URL + '/v1/tags/search/q:lizard' + userId;
+    const userResult = await makeRequest(path);
     return userResult;
   };
-  let conversationResult = await conversationsPath();
-  let messageResult = await messagesPath();
-  let userResult = await usersPath();
-  console.log('conversationResult>>>', conversationResult);
-  console.log('messageResult>>>', messageResult);
-  console.log('messageResult>>>', userResult);
+  let searchArray = JSON.parse(await conversationsPath());
+  console.log('conversationResult>>>', searchArray);
+
+
+  let setMessageId = await messagesPath(searchArray);
+  console.log('setMessageId>>>', setMessageId)
+
+  // let userResult = JSON.parse(await usersPath());
+  // console.log('userResult>>>', userResult);
 
 }
 getRecentConversationSummaries();
