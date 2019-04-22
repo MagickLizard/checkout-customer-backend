8b801ca3a016e7120beae478b4961c005ca7702c
Lizzie King <lizzieking521@gmail.com>
2019-04-22 19:32:58 +1000

    UPDATE: split paths into seperate functions

--- backend/services/api-request-service.js
+++ backend/services/api-request-service.js
@@ -2,7 +2,6 @@
 const request = require('request');
 const url = require('url');
 
-
 const API_BASE_URL = 'http://api.instagram.com';
 async function getRecentConversationSummaries() {
   let makeRequest = async (path) => {
@@ -22,44 +21,31 @@ async function getRecentConversationSummaries() {
       })
     })
   };
-  const conversationRequestWrapper = async (setMessagesPath) => {
+  const setConversationsPath = async () => {
     let path = API_BASE_URL;
     path += '/api/conversations';
     return makeRequest(path)
   };
-  const requestWrapper = async (conversationArray, requestType) => {
-    return new Promise((resolve, reject) => {
-      if (requestType === 1) {
-        path = API_BASE_URL + '/api/conversations/' + conversationArray + '/messages';
-      }
-      else if (requestType === 2) {
-        path = API_BASE_URL + '/api/users/' + conversationArray;
-      }
-      makeRequest(path)
-        .then(data => {
-          resolve(data);
-        })
-        .catch(err => {
-          console.error(' Error 2*****', err);
-          reject(err);
-        });
-    });
+  const setMessagePath = async (setMessagesPath) => {
+   let path = API_BASE_URL + '/api/conversations/' + setMessagesPath + '/messages';
+    return makeRequest(path)
   };
-  const conversationArray = await conversationRequestWrapper();
+  const setUsersPath = async (setMessagesPath) => {
+    let path = API_BASE_URL;
+    path += '/api/users/' + setMessagesPath;
+    return makeRequest(path)
+  };
+  const conversationArray = await setConversationsPath();
   const conversationMessages = await Promise.all(conversationArray.map(
-    (conversation) => (requestWrapper(conversation.id, 1))
+    (conversation) => (setMessagePath(conversation.id))
   ));
   let messageResult = (conversationMessages).map((k) => {
     for (let items of k) { return items; }
   });
   let messagesSorted = (messageResult).sort((previous, current) => current.created_at - previous.created_at);
   const userResult = await Promise.all(messagesSorted.map(
-    (conversation) => (requestWrapper(conversation.from_user_id, 2))
+    (conversation) => (setUsersPath(conversation.from_user_id))
   ));
-  console.log('conversationArray>>>', conversationArray)
-  console.log('messagesSorted>>>', messagesSorted)
-  console.log('userResult>>>', userResult)
-
   let conversationBody = [];
   const organiseData = (messagesSorted).reduce((previous, current, index) => {
           conversationBody.push({
@@ -76,8 +62,8 @@ async function getRecentConversationSummaries() {
           });
     return conversationBody;
   }, []);
-
-  return organiseData;
+console.log('organiseData>>>', JSON.stringify(organiseData))
+  return organiseData; //FORMAT DATA TO RESPONSE
 }
 // getRecentConversationSummaries();
 module.exports = getRecentConversationSummaries;
